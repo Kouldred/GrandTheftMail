f2b4552b75cf1d5050dc14ca96aff4edee5c6c28
Nikita Tikhomirov <nikita.tikhomirov@saludamedical.com>
2022-05-12 22:07:59 +0300

    Initial Commit

---
+++ MailDelivery/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MailDelivery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
